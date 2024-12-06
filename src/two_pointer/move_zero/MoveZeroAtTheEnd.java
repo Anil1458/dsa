28580e2c5f3fae35b843231c8565b101d605bf7b
anil <panditanil246@gmail.com>
2024-12-06 12:45:46 +0530

    move zeros at the end

---
+++ src/two_pointer/move_zero/MoveZeroAtTheEnd.java
@@ -0,0 +1,23 @@
+package two_pointer.move_zero;
+
+import java.util.Arrays;
+
+public class MoveZeroAtTheEnd {
+    public int[] moveZeroAtTheEnd(int[] arr){
+        int idx = 0;
+        for(int i : arr){
+            if(i != 0){
+                arr[idx++] = i;
+            }
+        }
+        while (idx < arr.length){
+            arr[idx++] = 0;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, -1, 0, 5, 0, 1, 0, 8};
+        System.out.println(Arrays.toString(new MoveZeroAtTheEnd().moveZeroAtTheEnd(arr)));
+    }
+}
